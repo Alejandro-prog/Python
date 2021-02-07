fad6a78ea7d98e4b781a18a4df09ad7b65e305d0
Alejandro <52725111+Alejandro-prog@users.noreply.github.com>
2021-02-07 10:59:53 -0500

    Create Matrices.py

---
+++ Matrices.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+h=np.array([[2,3,5,6,6],[6,7,33,5,6],[9,7,66,23,6],[14,5,44,12,6],[90,4,7,18,69]])
+
+print(h)
+
+print(h[3])
+print("\n")
+print(h[1:4])
+print("\n")
+print(h[1:])
+print("\n")
+print(h[:4])
+print("\n")
+print(h[:,[2]])
